8c233d180b68bdd089d344155058820514f5f698
ngoctu <ngoctu790@gmail.com>
2018-01-11 16:17:08 +0100

    update

--- master2/apprentissage_supervisé/projet/Projet_App-Sup_MLDS18/projet.R
+++ master2/apprentissage_supervisé/projet/Projet_App-Sup_MLDS18/projet.R
@@ -10,23 +10,70 @@ aggregation = read.table('/media/ngoctu/769829E69829A599/workspace/master2/maste
                          sep = '\t')
 
 # Donnees reelles: visa_premier, credit_card_fraud
-# visa_premier 
-# credit_card_fraud
+visapremier = read.table('/media/ngoctu/769829E69829A599/workspace/master2/master2/apprentissage_supervisé/projet/Projet_App-Sup_MLDS18/Data_projet_MLDS18/VisaPremier.txt', 
+                         header = T, 
+                         sep = '\t')
+creditcard = read.table('~/Downloads/Projet_App-Sup_MLDS18/Data_projet_MLDS18/creditcard.csv', header = T, sep = ',')
 
-data = flame
+# AUC scores
+auc.scores = data.frame(matrix(ncol = 11, nrow = 5))
+column.names = c('Dataset', 'Bayesien Naif', 'KNN', 'LDA', 
+                 'QDA', 'Linear SVM', 'Polynomial SVM', 
+                 'Gaussian SVM', 'Logistic Regression', 'CART', 'Random Forest')
+colnames(auc.scores) = column.names
+
+
+# Accuracy on test data
+accuracies = data.frame(matrix(ncol = 11, nrow = 5))
+colnames(accuracies) = column.names
+
+
+setwd('/media/ngoctu/769829E69829A599/workspace/master2/master2/apprentissage_supervisé/projet/Projet_App-Sup_MLDS18/3. aggregation/')
+
+data = spiral
+data[is.na(data)] = 0
+# data = data[, -1]
+name = 'spiral'
+row = 2
+
+auc.scores[row, 1] = name
+accuracies[row, 1] = name
+
+
+dim(data)
+View(data)
+
+# data$Amount = scale(data$Amount)
+
+col = c('anciente', 'mtrejet', 'nbopguic', 'moycred3', 'aveparmo', 'endette', 'engagemt', 'engagemc', 'engagemm', 'nbcptvue', 'moysold3', 'moycredi', 'agemvt', 'nbop', 'mtfactur', 'engageml', 'nbvie', 'mtvie', 'nbeparmo', 'mteparmo', 'nbeparlo', 'mteparlo', 'nblivret', 'mtlivret', 'nbeparlt', 'nbeparte', 'mteparte', 'nbbon', 'mtbon', 'nbpaiecb', 'nbcb', 'nbcbptar', 'avtscpte', 'aveparfi', 'nbjdebit')
+# X = data[, col]
+# y = as.factor(data$cartevpr)
+
 X = data[, -3]
 y = as.factor(data[, 3])
-
 summary(X)
 summary(y)
-boxplot(X)
-hist(X$V1)
-hist(X$V2)
+
+png(paste(name,'.png'), width = 800, height = 600, units = 'px')
+plot(X, col = y, main = name)
+dev.off()
+
+png(paste(name,'.boxplot.png'), width = 800, height = 600, units = 'px')
+boxplot(X, main = name)
+dev.off()
+
+png(paste(name,'.histogram.png'), width = 800, height = 600, units = 'px')
+par(mfrow = c(1, 2))
+hist(X$V1, main = paste('Histogram of ',name, "'s V1"))
+hist(X$V2, main = paste('Histogram of ',name, "'s V2"))
+dev.off()
+
 
 cl = cor(X)
 library(corrplot)
+png(paste(name,'.cor.png'), width = 800, height = 600, units = 'px')
 corrplot(cl, method = 'square')
-plot(X, col = y)
+dev.off()
 
 
 # Splitting train-val-test (60-20-20)
@@ -39,16 +86,6 @@ y_test = y[-tr_val]
 X_train_val = X[tr_val, ]
 y_train_val = y[tr_val]
 
-# Get the shift and scale from the training data
-X_train_val_std = apply(X_train_val, 2, sd)
-X_train_val_mean = colMeans(X_train_val)
-
-# Normalize train data
-X_train_val = scale(X_train_val)
-
-# Normalize test data
-X_test = scale(X_test, scale = X_train_val_std, center = X_train_val_mean)
-
 num_train = as.integer(0.6 * nrow(X))
 tr = sample(1:nrow(X_train_val), num_train)
 
@@ -58,9 +95,20 @@ y_train = y_train_val[tr]
 X_val = X_train_val[-tr, ]
 y_val = y_train_val[-tr]
 
+# Normalization
+# Get the shift and scale from the training data
+X_train_val_std = apply(X_train_val, 2, sd)
+X_train_val_mean = colMeans(X_train_val)
 
+# Normalize train data
+X_train_val = scale(X_train_val)
+X_train = scale(X_train, scale = X_train_val_std, center = X_train_val_mean)
+X_val = scale(X_val, scale = X_train_val_std, center = X_train_val_mean)
 
+# Normalize test data
+X_test = scale(X_test, scale = X_train_val_std, center = X_train_val_mean)
 
+  
 # ROC curve
 library(ROCR)
 
@@ -69,11 +117,12 @@ library(ROCR)
 library(e1071)
 nb = naiveBayes(X_train_val, y_train_val)
 nb.prob = predict(nb, X_test, type = 'raw')
+# nb.prob = predict(nb, X_test)
 nb.preds = prediction(nb.prob[, 2], y_test)
 nb.roc = performance(nb.preds, 'tpr', 'fpr')
 nb.auc = performance(nb.preds, measure = 'auc')
-nb.auc@y.values
-nb.test_accuracy = mean(predict(nb, X_test) == y_test)
+auc.scores[row, 2] = nb.auc@y.values[[1]]
+accuracies[row, 2] = mean(predict(nb, X_test) == y_test)
 
 
 # 2. KNN
@@ -86,19 +135,22 @@ for (k in 1:kmax){
   pred = knn(X_train, X_val, y_train, k)
   err_val[k] = sum(pred != y_val) / length(y_val)
 }
-plot(err_val, type = "l", col = 2, xlab = "nombre de voisins", 
-     ylab = "taux d'erreur") 
-K = which.min(err_val[-1]) 
+# plot(err_val, type = "l", col = 2, xlab = "nombre de voisins", 
+#     ylab = "taux d'erreur") 
+K = which.min(err_val) 
 
 knn.predictions = knn(X_train_val, X_test, y_train_val, K, prob = T)
+# knn.predictions = knn(X_train_val, X_test, y_train_val, K)
+
 knn.prob = attr(knn.predictions, 'prob')
-knn.prob <- 2*ifelse(knn.predictions == 1, 1 - knn.prob, knn.prob) - 1
+knn.prob <- 2*ifelse(knn.predictions == 0, 1 - knn.prob, knn.prob) - 1
 knn.pred = prediction(knn.prob, y_test)
 knn.roc = performance(knn.pred, 'tpr', 'fpr')
 knn.auc = performance(knn.pred, measure = 'auc')
-knn.auc@y.values
-plot(knn.roc)
-# knn.test_accuracy = mean(knn(X_train_val, X_test, y_train_val, K) == y_test)
+auc.scores[row, 3] = knn.auc@y.values[[1]]
+
+# plot(knn.roc)
+accuracies[row, 3] = mean(knn(X_train_val, X_test, y_train_val, K) == y_test)
 
 
 # 3. LDA
@@ -108,9 +160,9 @@ lda.prob = predict(lda.res, X_test)$posterior
 lda.pred = prediction(lda.prob[, 2], y_test)
 lda.roc = performance(lda.pred, 'tpr', 'fpr')
 lda.auc = performance(lda.pred, measure = 'auc')
-lda.auc@y.values
-mean(predict(lda.res, X_test)$class == y_test)
-plot(lda.roc)
+auc.scores[row, 4] =lda.auc@y.values[[1]]
+accuracies[row, 4] = mean(predict(lda.res, X_test)$class == y_test)
+# plot(lda.roc)
 
 
 # 4. QDA?
@@ -119,18 +171,18 @@ qda.prob = predict(qda.res, X_test)$posterior
 qda.pred = prediction(qda.prob[, 2], y_test)
 qda.roc = performance(qda.pred, 'tpr', 'fpr')
 qda.auc = performance(qda.pred, measure = 'auc')
-qda.auc@y.values
-mean(predict(qda.res, X_test)$class == y_test)
-plot(qda.roc)
+auc.scores[row, 5] = qda.auc@y.values[[1]]
+accuracies[row, 5] = mean(predict(qda.res, X_test)$class == y_test)
+# plot(qda.roc)
 
 
 # SVM
-costs = c(1e+6, 1e+3, 1, 1e-3)
+costs = c(1e+3, 1, 1e-3)
 degrees = c(2, 3, 5, 10)
-gammas = c(1, 1e-3, 1e-5)
+gammas = c(1e-3, 1e-5)
 
 # 5. Linear SVM
-best_cost = 0.0
+best_cost = 1
 best_accuracy = 0.0
 for (cost in costs){
   lin.svm.res = svm(X_train, y_train, kernel = 'linear', cost = cost)
@@ -142,13 +194,14 @@ for (cost in costs){
 }
 
 lin.svm.res = svm(X_train_val, y_train_val, kernel = 'linear', cost = best_cost, probability = TRUE)
+# lin.svm.res = svm(X_train_val, y_train_val, kernel = 'linear', cost = best_cost)
 lin.svm.prob = attr(predict(lin.svm.res, X_test, probability = T), 'probabilities')
-lin.svm.pred = prediction(lin.svm.prob[, 1], y_test)
+lin.svm.pred = prediction(lin.svm.prob[, 2], y_test)
 lin.svm.roc = performance(lin.svm.pred, 'tpr', 'fpr')
 lin.svm.auc = performance(lin.svm.pred, measure = 'auc')
-lin.svm.auc@y.values
-mean(predict(lin.svm.res, X_test)== y_test)
-plot(lin.svm.roc)
+auc.scores[row, 6] = lin.svm.auc@y.values[[1]]
+accuracies[row, 6] = mean(predict(lin.svm.res, X_test) == y_test)
+# plot(lin.svm.roc)
 
 # 6. Non-linear SVM
 # 6.1. Polynomial SVM
@@ -172,14 +225,17 @@ for (cost in costs){
 }
 
 poly.svm.res = svm(X_train_val, y_train_val, kernel = 'polynomial', 
-                   cost = best_cost, gamma = best_gamma, degree = best_degree, probability = TRUE)
+                  cost = best_cost, gamma = best_gamma, degree = best_degree, probability = TRUE)
+
+# poly.svm.res = svm(X_train_val, y_train_val, kernel = 'polynomial', 
+#                    cost = best_cost, gamma = best_gamma, degree = best_degree)
 poly.svm.prob = attr(predict(poly.svm.res, X_test, probability = T), 'probabilities')
-poly.svm.pred = prediction(poly.svm.prob[, 1], y_test)
+poly.svm.pred = prediction(poly.svm.prob[, 2], y_test)
 poly.svm.roc = performance(poly.svm.pred, 'tpr', 'fpr')
 poly.svm.auc = performance(poly.svm.pred, measure = 'auc')
-poly.svm.auc@y.values
-mean(predict(poly.svm.res, X_test)== y_test)
-plot(poly.svm.roc)
+auc.scores[row, 7] = poly.svm.auc@y.values[[1]]
+accuracies[row, 7] = mean(predict(poly.svm.res, X_test)== y_test)
+# plot(poly.svm.roc)
 
 # 6.2. Radial basis
 best_cost = 0.0
@@ -200,26 +256,34 @@ for (cost in costs){
 
 rbf.svm.res = svm(X_train_val, y_train_val, kernel = 'radial', 
                   cost = best_cost, gamma = best_gamma, degree = best_degree, probability = TRUE)
+#rbf.svm.res = svm(X_train_val, y_train_val, kernel = 'radial', 
+#                  cost = best_cost, gamma = best_gamma, degree = best_degree)
 rbf.svm.prob = attr(predict(rbf.svm.res, X_test, probability = T), 'probabilities')
-rbf.svm.pred = prediction(rbf.svm.prob[, 1], y_test)
+rbf.svm.pred = prediction(rbf.svm.prob[, 2], y_test)
 rbf.svm.roc = performance(rbf.svm.pred, 'tpr', 'fpr')
 rbf.svm.auc = performance(rbf.svm.pred, measure = 'auc')
-rbf.svm.auc@y.values
-mean(predict(rbf.svm.res, X_test)== y_test)
-plot(rbf.svm.roc)
+auc.scores[row, 8] = rbf.svm.auc@y.values[[1]]
+accuracies[row, 8] = mean(predict(rbf.svm.res, X_test) == y_test)
+# plot(rbf.svm.roc)
 
 # 7. Logistic Regression
+library(nnet)
+glm.fit = multinom(y_train_val ~ ., as.data.frame(X_train_val))
 glm.fit = glm(y_train_val ~ ., as.data.frame(X_train_val), family = 'binomial')
 glm.prob = predict(glm.fit, as.data.frame(X_test), type = 'response')
 glm.pred = prediction(glm.prob, y_test)
 glm.roc = performance(glm.pred, 'tpr', 'fpr')
 glm.auc = performance(glm.pred, measure = 'auc')
-glm.auc@y.values
-plot(glm.roc)
+
+# plot(glm.roc)
 
 glm.prediction = rep(1, length(y_test))
 glm.prediction[glm.prob > 0.5] = 2
-mean(glm.prediction == y_test)
+accuracies[row, 9] = mean(glm.prediction == y_test)
+auc.scores[row, 9] = glm.auc@y.values[[1]]
+
+# multiclass
+accuracies[row, 9] = mean(predict(glm.fit, X_test) == y_test)
 
 
 # 8. CART
@@ -229,17 +293,39 @@ cart.prob = predict(cart.res, as.data.frame(X_test))
 cart.pred = prediction(cart.prob[, 2], y_test)
 cart.roc = performance(cart.pred, 'tpr', 'fpr')
 cart.auc = performance(cart.pred, measure = 'auc')
-cart.auc@y.values
-plot(cart.roc)
-mean(predict(cart.res, as.data.frame(X_test), type = 'class') == y_test)
+auc.scores[row, 10] = cart.auc@y.values[[1]]
+# plot(cart.roc)
+accuracies[row, 10] = mean(predict(cart.res, as.data.frame(X_test), type = 'class') == y_test)
 
 # 9. Random Forest
 library(randomForest)
-rf.res = randomForest(X_train_val, y_train_val, ntree = 100, mtry = 2, importance = T)
+
+# number of features: 1 + log2(M), M: number of variables, according to Breiman
+rf.res = randomForest(X_train_val, y_train_val, ntree = 100, mtry = as.integer(1 + log2(dim(X)[2])), importance = T)
 rf.prob = predict(rf.res, X_test, type = 'prob')
 rf.pred = prediction(rf.prob[, 2], y_test)
 rf.roc = performance(rf.pred, 'tpr', 'fpr')
 rf.auc = performance(rf.pred, measure = 'auc')
-rf.auc@y.values
-mean(predict(cart.res, as.data.frame(X_test), type = 'class') == y_test)
-plot(rf.roc)
+auc.scores[row, 11] = rf.auc@y.values[[1]]
+accuracies[row, 11] = mean(predict(cart.res, as.data.frame(X_test), type = 'class') == y_test)
+
+par("mar")
+par(mar=c(1,1,1,1))
+png(paste(name,'.roc.png'), width = 800, height = 600, units = 'px')
+par(mfrow = c(2, 5))
+plot(nb.roc, main = 'Bayesien Naif')
+plot(knn.roc, main = 'KNN')
+plot(lda.roc, main = 'LDA')
+plot(qda.roc, main = 'QDA')
+plot(lin.svm.roc, main = 'Linear SVM')
+plot(poly.svm.roc, main = 'Polynomial SVM')
+plot(rbf.svm.roc, main = 'Gaussian SVM')
+plot(glm.roc, main = 'Logistic Regression')
+plot(cart.roc, main = 'CART')
+plot(rf.roc, main = 'Random Forest')
+dev.off()
+
+auc.scores
+write.csv(auc.scores, file = 'AUC.csv')
+accuracies
+write.csv(accuracies, file = 'accuracies.csv')
--- master2/exploration_visuelle_des_donnees/Projet_Visualisation/projet.R
+++ master2/exploration_visuelle_des_donnees/Projet_Visualisation/projet.R
@@ -3,11 +3,12 @@
 # gordon
 data = read.table('/media/ngoctu/769829E69829A599/workspace/master2/master2/exploration_visuelle_des_donnees/Projet_Visualisation/gordon-2002_database.txt', 
                   sep = '\t')
+
 X = t(data[-(1:2), ])[-1, ]
 shape = dim(X)
 y = t(data[2, ])
 y = y[-1]
- y = as.factor(y)
+y = as.factor(y)
 
 X = mapply(X, FUN = as.numeric)
 X = matrix(data = X, nrow = shape[1], ncol = shape[2])
@@ -16,11 +17,11 @@ summary(y)
 
 X = scale(X, center = T, scale = T)
 
-df = data.frame(X, y)
+gordon = data.frame(X, y)
 set.seed(2018)
 tr = sample(1:nrow(X), as.integer(0.8 * nrow(X)))
-train = df[tr, ]
-test = df[-tr, ]
+train = gordon[tr, ]
+test = gordon[-tr, ]
 
 # Methodes
 # 1. ACP
@@ -29,18 +30,68 @@ res.pca = PCA(gordon, scale.unit = T, ncp = 2, quali.sup = 1627)
 plot.PCA(res.pca, choix = 'ind', habillage = 1627)
 
 # 2. ADL
-library(mda)
 library(MASS)
 # res.adl = fda(y ~ ., data = gordon[, -1627], dimension = 2)
 res.adl = lda(y ~., data = train)
 predict.adl = predict(res.adl, test[, -1627])
 table(predict.adl$class, test[, 1627])
 
-projected_data = as.matrix(df[, -1627]) %*% res.adl$scaling
-plot(projected_data, col = df[, 1627], pch = 19)
+projected_data = as.matrix(gordon[, -1627]) %*% res.adl$scaling
+plot(projected_data, col = gordon[, 1627], pch = 19)
 
 # 3. MDS
+D = dist(gordon[, -1627])
+res.mds = cmdscale(D)
+plot(res.mds, col = gordon[, 1627], pch = 19)
 
 # 4. Isomap
+library(RDRToolbox)
+par(mfrow=c(3, 3))
+K = c(2, 3, 5, 10, 15, 20, 30, 50, 100)
+for (k in K){
+  res.isomap = Isomap(as.matrix(gordon[, -1627]), k = k)
+  plot(res.isomap$dim2, col = gordon[, 1627], pch = 19, main = paste('K =',k), xlab = 'MDS dim1', ylab = 'MDS dim2')
+}
+
 # 5. LLE
+par(mfrow=c(3, 3))
+for (k in K){
+  res.lle = LLE(as.matrix(gordon[, -1627]), dim = 2, k = k)
+  plot(res.lle, col = gordon[, 1627], pch = 19, main = paste('K =', k), xlab = 'MDS dim1', ylab = 'MDS dim2')
+}
+
+
 # 6. SOM
+library(kohonen)
+res.som = som(as.matrix(gordon[, -1627]), grid = somgrid(10, 10, topo = 'hexagonal'))
+res.som$codes
+res.som$distances
+res.som$changes
+color1 = tricolor(res.som$grid)
+plot(res.som, type = 'mapping', bgcol = rgb(color1))
+plot(res.som, type = 'mapping', col = gordon[, 1627], pch = 19)
+plot(res.som, type = 'quality')
+
+# Node counts
+plot(res.som, type = 'counts')
+plot(res.som, type = 'codes')
+
+coolBlueHotRed = function(n, alpha = 1)
+{
+  rainbow(n, end=4/6, alpha=alpha)[n:1]
+}
+
+# U-matrix or Neighbour distance
+plot(res.som, type = 'dist.neighbours')
+
+mydata <- res.som$codes[[1]]
+wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var)) 
+for (i in 2:15) {
+  wss[i] <- sum(kmeans(mydata, centers=i)$withinss)
+}
+plot(wss)
+## use hierarchical clustering to cluster the codebook vectors
+som_cluster <- cutree(hclust(dist(res.som$codes[[1]])), 2)
+# plot these results:
+plot(res.som, type="mapping", bgcol = som_cluster, main = "Clusters") 
+add.cluster.boundaries(som_model, som_cluster)
